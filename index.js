bfd3cbd63fe708545d8447d7991f848d51fc1547
shohan-ontik <shohan@ontik.technology>
2026-01-20 18:31:14 +0600

    Track player removed

--- index.js
+++
@@ -1,9 +0,0 @@
-import { registerRootComponent } from "expo";
-import { ExpoRoot } from "expo-router";
-import TrackPlayer from "react-native-track-player";
-
-TrackPlayer.registerPlaybackService(
-  () => require("./components/music-player/setup/playbackService").default,
-);
-
-registerRootComponent(ExpoRoot);
